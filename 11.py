a97cf9a21844a9e71cc52beaabbf74f091cf233b
spossner <github@possner.de>
2021-12-05 19:47:24 +0100

    maze solving puzzle

--- 11.py
+++ 11.py
@@ -2,19 +2,20 @@ import re
 from itertools import chain, combinations
 from collections import deque, Counter
 
+
 class Solution:
     def __init__(self, data) -> None:
         self.floors = [set(re.findall(r'(\w+)(?:-compatible)? (microchip|generator)', line)) for line in data]
         # PART II
         self.floors[0] = self.floors[0].union([('elerium', 'generator'), ('elerium', 'microchip'),
-                                 ('dilithium', 'generator'), ('dilithium', 'microchip')])
+                                               ('dilithium', 'generator'), ('dilithium', 'microchip')])
         print(self.floors)
 
     def is_valid_transition(self, floor):
         return len(set(type for _, type in floor)) < 2 or \
-            all((obj, 'generator') in floor
-                for (obj, type) in floor
-                if type == 'microchip')
+               all((obj, 'generator') in floor
+                   for (obj, type) in floor
+                   if type == 'microchip')
 
     def next_states(self, state):
         moves, elevator, floors = state
@@ -33,11 +34,11 @@ class Solution:
 
                 if (self.is_valid_transition(next_floors[elevator]) and self.is_valid_transition(next_floors[next_elevator])):
                     yield (moves + 1, next_elevator, next_floors)
-    
+
     def is_all_top_level(self, floors):
         return all(not floor
-            for number, floor in enumerate(floors)
-            if number < len(floors) - 1)
+                   for number, floor in enumerate(floors)
+                   if number < len(floors) - 1)
 
     def count_floor_objects(self, state):
         _, elevator, floors = state
@@ -62,8 +63,6 @@ class Solution:
 
 
 if __name__ == '__main__':
-    
     with open('11.txt') as f:
         s = Solution(f.read().strip().splitlines())
         print(s.solve())
-
